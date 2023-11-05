f53374ae6f4d86bf6dd58aa4a29c6ddf0c9754eb
Ine Ballardin <ineballardin@gmail.com>
2023-11-05 18:49:10 -0300

    create grpc server

--- internal/service/category.go
+++ internal/service/category.go
@@ -2,10 +2,9 @@ package service
 
 import (
 	"context"
-	"io"
 
-	"github.com/devfullcycle/14-gRPC/internal/database"
-	"github.com/devfullcycle/14-gRPC/internal/pb"
+	"github.com/ineBallardin/gRPC/internal/database"
+	"github.com/ineBallardin/gRPC/internal/pb"
 )
 
 type CategoryService struct {
@@ -34,89 +33,89 @@ func (c *CategoryService) CreateCategory(ctx context.Context, in *pb.CreateCateg
 	return categoryResponse, nil
 }
 
-func (c *CategoryService) ListCategories(ctx context.Context, in *pb.Blank) (*pb.CategoryList, error) {
-	categories, err := c.CategoryDB.FindAll()
-	if err != nil {
-		return nil, err
-	}
+// func (c *CategoryService) ListCategories(ctx context.Context, in *pb.Blank) (*pb.CategoryList, error) {
+// 	categories, err := c.CategoryDB.FindAll()
+// 	if err != nil {
+// 		return nil, err
+// 	}
 
-	var categoriesResponse []*pb.Category
+// 	var categoriesResponse []*pb.Category
 
-	for _, category := range categories {
-		categoryResponse := &pb.Category{
-			Id:          category.ID,
-			Name:        category.Name,
-			Description: category.Description,
-		}
+// 	for _, category := range categories {
+// 		categoryResponse := &pb.Category{
+// 			Id:          category.ID,
+// 			Name:        category.Name,
+// 			Description: category.Description,
+// 		}
 
-		categoriesResponse = append(categoriesResponse, categoryResponse)
-	}
+// 		categoriesResponse = append(categoriesResponse, categoryResponse)
+// 	}
 
-	return &pb.CategoryList{Categories: categoriesResponse}, nil
-}
+// 	return &pb.CategoryList{Categories: categoriesResponse}, nil
+// }
 
-func (c *CategoryService) GetCategory(ctx context.Context, in *pb.CategoryGetRequest) (*pb.Category, error) {
-	category, err := c.CategoryDB.Find(in.Id)
-	if err != nil {
-		return nil, err
-	}
+// func (c *CategoryService) GetCategory(ctx context.Context, in *pb.CategoryGetRequest) (*pb.Category, error) {
+// 	category, err := c.CategoryDB.Find(in.Id)
+// 	if err != nil {
+// 		return nil, err
+// 	}
 
-	categoryResponse := &pb.Category{
-		Id:          category.ID,
-		Name:        category.Name,
-		Description: category.Description,
-	}
+// 	categoryResponse := &pb.Category{
+// 		Id:          category.ID,
+// 		Name:        category.Name,
+// 		Description: category.Description,
+// 	}
 
-	return categoryResponse, nil
-}
+// 	return categoryResponse, nil
+// }
 
-func (c *CategoryService) CreateCategoryStream(stream pb.CategoryService_CreateCategoryStreamServer) error {
-	categories := &pb.CategoryList{}
+// func (c *CategoryService) CreateCategoryStream(stream pb.CategoryService_CreateCategoryStreamServer) error {
+// 	categories := &pb.CategoryList{}
 
-	for {
-		category, err := stream.Recv()
-		if err == io.EOF {
-			return stream.SendAndClose(categories)
-		}
-		if err != nil {
-			return err
-		}
+// 	for {
+// 		category, err := stream.Recv()
+// 		if err == io.EOF {
+// 			return stream.SendAndClose(categories)
+// 		}
+// 		if err != nil {
+// 			return err
+// 		}
 
-		categoryResult, err := c.CategoryDB.Create(category.Name, category.Description)
-		if err != nil {
-			return err
-		}
+// 		categoryResult, err := c.CategoryDB.Create(category.Name, category.Description)
+// 		if err != nil {
+// 			return err
+// 		}
 
-		categories.Categories = append(categories.Categories, &pb.Category{
-			Id:          categoryResult.ID,
-			Name:        categoryResult.Name,
-			Description: categoryResult.Description,
-		})
-	}
-}
+// 		categories.Categories = append(categories.Categories, &pb.Category{
+// 			Id:          categoryResult.ID,
+// 			Name:        categoryResult.Name,
+// 			Description: categoryResult.Description,
+// 		})
+// 	}
+// }
 
-func (c *CategoryService) CreateCategoryStreamBidirectional(stream pb.CategoryService_CreateCategoryStreamBidirectionalServer) error {
-	for {
-		category, err := stream.Recv()
-		if err == io.EOF {
-			return nil
-		}
-		if err != nil {
-			return err
-		}
+// func (c *CategoryService) CreateCategoryStreamBidirectional(stream pb.CategoryService_CreateCategoryStreamBidirectionalServer) error {
+// 	for {
+// 		category, err := stream.Recv()
+// 		if err == io.EOF {
+// 			return nil
+// 		}
+// 		if err != nil {
+// 			return err
+// 		}
 
-		categoryResult, err := c.CategoryDB.Create(category.Name, category.Description)
-		if err != nil {
-			return err
-		}
+// 		categoryResult, err := c.CategoryDB.Create(category.Name, category.Description)
+// 		if err != nil {
+// 			return err
+// 		}
 
-		err = stream.Send(&pb.Category{
-			Id:          categoryResult.ID,
-			Name:        categoryResult.Name,
-			Description: categoryResult.Description,
-		})
-		if err != nil {
-			return err
-		}
-	}
-}
+// 		err = stream.Send(&pb.Category{
+// 			Id:          categoryResult.ID,
+// 			Name:        categoryResult.Name,
+// 			Description: categoryResult.Description,
+// 		})
+// 		if err != nil {
+// 			return err
+// 		}
+// 	}
+// }
